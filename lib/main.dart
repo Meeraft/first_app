9fe7675488cf9f7f992e933cfa2a07b6b60feb1c
Meeraft <hamsaniteen@gmail.com>
2026-04-13 19:18:43 +0800

    shupp

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp
-  (MaterialApp
-  (home: Scaffold( backgroundColor: Colors.yellow, body:(Text ('MUHAMMAD HAMSANI MIKAIL BIN SHAHRIL',style: TextStyle(fontSize: 50),
-)
-),
-  appBar: AppBar(  backgroundColor: Colors.blueGrey,
-    title: Center(child: Text('HELLO WORLD USAS')),
-  ),
-)
-)
-);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        body: Center(
+          child: SafeArea(
+            child: Container(
+              color: Colors.white, child: Text('MUHAMMAD HAMSANI MIKAIL BIN SHAHRIL', 
+              style: TextStyle(color: Colors.yellow, fontWeight: FontWeight(1000)),)
+              ,alignment: Alignment.center, width: 350, height: 500, margin: EdgeInsets.symmetric(
+              vertical: 10,
+                  horizontal: 10
+            ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
